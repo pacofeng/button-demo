3b3155e3a082eadb8ea36ddc6ea6ef4f05e95d35
pacofeng86@gmail.com <pacofeng86@gmail.com>
2023-12-18 16:34:30 +0800

    doc: auto-complete

--- src/components/Autocomplete/Autocomplete.stories.tsx
+++ src/components/Autocomplete/Autocomplete.stories.tsx
@@ -13,7 +13,11 @@ export default meta;
 type Story = StoryObj<typeof Autocomplete>;
 
 const films = [
-  { label: "The Shawshank Redemption Redemption Redemption", year: 1994 },
+  { label: "The Shawshank Redemption ", year: 1994 },
+  {
+    label: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+  },
   { label: "The Godfather", year: 1972 },
   { label: "The Godfather: Part II", year: 1974 },
   { label: "The Dark Knight", year: 2008 },
@@ -24,6 +28,34 @@ export const Small: Story = {
   render: () => (
     <Autocomplete
       size="small"
+      sx={{ width: 200 }}
+      options={films}
+      renderInput={(params) => (
+        <TextField {...params} label="Movie" size="small" />
+      )}
+    />
+  ),
+};
+
+export const SmallFilled: Story = {
+  render: () => (
+    <Autocomplete
+      size="small"
+      sx={{ width: 200 }}
+      options={films}
+      defaultValue="The Lord of the Rings: The Return of the King"
+      renderInput={(params) => (
+        <TextField {...params} label="Movie" size="small" />
+      )}
+    />
+  ),
+};
+
+export const SmallDisabled: Story = {
+  render: () => (
+    <Autocomplete
+      size="small"
+      disabled
       sx={{ width: 200 }}
       options={films}
       renderInput={(params) => (
@@ -46,10 +78,66 @@ export const Medium: Story = {
   ),
 };
 
+export const MediumFilled: Story = {
+  render: () => (
+    <Autocomplete
+      size="medium"
+      sx={{ width: 200 }}
+      options={films}
+      defaultValue="The Lord of the Rings: The Return of the King"
+      renderInput={(params) => (
+        <TextField {...params} label="Movie" size="medium" />
+      )}
+    />
+  ),
+};
+
+export const MediumDisabled: Story = {
+  render: () => (
+    <Autocomplete
+      size="medium"
+      disabled
+      sx={{ width: 200 }}
+      options={films}
+      renderInput={(params) => (
+        <TextField {...params} label="Movie" size="medium" />
+      )}
+    />
+  ),
+};
+
 export const Large: Story = {
   render: () => (
     <Autocomplete
       size="large"
+      sx={{ width: 200 }}
+      options={films}
+      renderInput={(params) => (
+        <TextField {...params} label="Movie" size="large" />
+      )}
+    />
+  ),
+};
+
+export const LargeFilled: Story = {
+  render: () => (
+    <Autocomplete
+      size="large"
+      sx={{ width: 200 }}
+      options={films}
+      defaultValue="The Lord of the Rings: The Return of the King"
+      renderInput={(params) => (
+        <TextField {...params} label="Movie" size="large" />
+      )}
+    />
+  ),
+};
+
+export const LargeDisabled: Story = {
+  render: () => (
+    <Autocomplete
+      size="large"
+      disabled
       sx={{ width: 200 }}
       options={films}
       renderInput={(params) => (
