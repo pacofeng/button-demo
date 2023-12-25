16c1480b31b01abc2336d1745dc58f7ca46f9ca5
pacofeng86@gmail.com <pacofeng86@gmail.com>
2023-12-25 14:45:34 +0800

    feat: replace focus with hover

--- src/components/Autocomplete/Autocomplete.tsx
+++ src/components/Autocomplete/Autocomplete.tsx
@@ -20,18 +20,32 @@ export const Autocomplete = styled(
         "& .MuiAutocomplete-paper": {
           fontSize:
             props.size === "small" ? 10 : props.size === "large" ? 14 : 12,
+          letterSpacing: props.size === "large" ? 0.1 : 0.25,
+          lineHeight:
+            props.size === "small"
+              ? "14px"
+              : props.size === "large"
+              ? "20px"
+              : "16px",
           "& .MuiAutocomplete-option": {
-            "&.Mui-focused": {
-              backgroundColor: "#E7ECF0",
-            },
-            "&[aria-selected='true']:not(.Mui-focused)": {
+            "&[aria-selected='true']": {
               backgroundColor: "#F7F7F8",
             },
             "&[aria-disabled='true']": {
               color: "#D6D8DA",
               opacity: 1,
-              "&.Mui-focused": {
+            },
+            // use hover instead of Mui-focused to update default bg color when dropdown opens
+            "&.Mui-focused": {
+              backgroundColor: "transparent",
+              "&[aria-selected='true']": {
                 backgroundColor: "#F7F7F8",
+              },
+            },
+            "&:hover": {
+              backgroundColor: "#E7ECF0",
+              "&[aria-selected='true']": {
+                backgroundColor: "#E7ECF0",
               },
             },
           },
@@ -46,7 +60,7 @@ export const Autocomplete = styled(
     "&.MuiAutocomplete-hasPopupIcon": {
       "& .MuiAutocomplete-inputRoot.MuiOutlinedInput-root": {
         paddingRight:
-          props.size === "small" ? 24 : props.size === "large" ? 34 : 27,
+          props.size === "small" ? 24 : props.size === "large" ? 34 : 28,
       },
       "&.MuiAutocomplete-hasClearIcon": {
         "& .MuiAutocomplete-inputRoot.MuiOutlinedInput-root": {
