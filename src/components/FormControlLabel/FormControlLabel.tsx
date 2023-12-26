6a81eed96af6677190ca9fd6bfb8aec3867900ed
lyn <ningshao01@outlook.com>
2023-12-26 18:01:22 +0800

    feat: 添加disabled时文本颜色变更 & hover到文本时background变更

--- src/components/FormControlLabel/FormControlLabel.tsx
+++ src/components/FormControlLabel/FormControlLabel.tsx
@@ -16,7 +16,7 @@ export const FormControlLabel: FC<FormControlLabelProps> = styled(
   ".MuiFormControlLabel-label.Mui-disabled": {
     color: "#333D47",
   },
-  // checkbox, radio and witch
+  // checkbox, radio and switch
   ".MuiCheckbox-root, .MuiRadio-root, .MuiSwitch-root": {
     "+ .MuiFormControlLabel-label": {
       fontSize: props.size === "small" ? 10 : props.size === "large" ? 14 : 12,
@@ -27,6 +27,10 @@ export const FormControlLabel: FC<FormControlLabelProps> = styled(
           ? "20px"
           : "16px",
       letterSpacing: props.size === "large" ? 0.1 : 0.25,
+    },
+
+    "+ .MuiFormControlLabel-label.Mui-disabled": {
+      color: "#5B636B",
     },
   },
   "&:hover": {
@@ -41,6 +45,15 @@ export const FormControlLabel: FC<FormControlLabelProps> = styled(
           color: "#0F3556",
         },
       },
+    "& .MuiSwitch-root:not(.Mui-disabled)": {
+      "& .MuiSwitch-track": {
+        backgroundColor: "#444",
+      },
+
+      "& .MuiSwitch-switchBase.Mui-checked+.MuiSwitch-track": {
+        backgroundColor: "#0f3556",
+      },
+    },
   },
 }));
 
