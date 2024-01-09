a1188756802ca00432d084b82a4cc3d054441d78
pacofeng86@gmail.com <pacofeng86@gmail.com>
2024-01-09 09:43:21 +0800

    update tabs

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -12,30 +12,32 @@ export const Tabs: FC<TabsProps> = styled(MTabs)((props: TabsProps) => ({
     height: 3,
   },
   ".MuiTab-root": {
-    ...(props.size === "small" && {
-      fontSize: 10,
-      height: 28,
-      lineHeight: "14px",
-      ".MuiSvgIcon-root": {
-        fontSize: 12,
-      },
-    }),
-    ...((props.size === "medium" || !props.size) && {
-      fontSize: 12,
-      height: 32,
-      lineHeight: "16px",
-      ".MuiSvgIcon-root": {
-        fontSize: 16,
-      },
-    }),
-    ...(props.size === "large" && {
-      fontSize: 14,
-      height: 36,
-      lineHeight: "20px",
-      ".MuiSvgIcon-root": {
-        fontSize: 16,
-      },
-    }),
+    ...(props.size === "small"
+      ? {
+          fontSize: 10,
+          height: 28,
+          lineHeight: "14px",
+          ".MuiSvgIcon-root": {
+            fontSize: 12,
+          },
+        }
+      : props.size === "large"
+      ? {
+          fontSize: 14,
+          height: 36,
+          lineHeight: "20px",
+          ".MuiSvgIcon-root": {
+            fontSize: 16,
+          },
+        }
+      : {
+          fontSize: 12,
+          height: 32,
+          lineHeight: "16px",
+          ".MuiSvgIcon-root": {
+            fontSize: 16,
+          },
+        }),
   },
 }));
 
